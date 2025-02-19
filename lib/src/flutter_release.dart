696ebbc45376a803019d09dd10908290db34e424
lamnhan066 <hello@lamnhan.dev>
2025-02-19 12:05:36 +0700

    Separate the `runFlutterRelease` method and return as bool

---
+++ lib/src/flutter_release.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'utils.dart';
+
+/// Run Flutter release
+bool runFlutterRelease(bool isVerbose, String releasePath) {
+  final r = Process.runSync('flutter', [
+    'build',
+    'macos',
+    '--release',
+    '--obfuscate',
+    '--split-debug-info=${joinPaths(['.', 'build', 'debug-macos-info'])}',
+  ]);
+
+  final appPath = getAppPath(releasePath);
+
+  if (appPath == '' || isVerbose) {
+    print(r.stdout);
+  }
+
+  return appPath.isNotEmpty;
+}
+
+/// Delete build of macos
+void cleanBuild(bool isVerbose) {
+  final build = Directory(joinPaths(['.', 'build', 'macos']));
+  if (!build.existsSync()) return;
+
+  build.deleteSync(recursive: true);
+}
